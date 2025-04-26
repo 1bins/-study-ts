777bbe0166fda483acb113068461d8d4d3f0a428
서원빈 <1bins@kakao.com>
2025-04-26 12:28:44 +0900

    원시타입과 리터럴타입

---
+++ section2/src/chapter1.ts
@@ -0,0 +1,30 @@
+// number
+let num1: number = 123;
+let num2: number = -123;
+let num3: number = 0.123;
+let num4: number = Infinity;
+let num5: number = -Infinity;
+let num6: number = NaN;
+
+// string
+let str1: string = "hello";
+let str2: string = 'hello';
+let str3: string = `hello`;
+let str4: string = `hello ${num1}`;
+
+// boolean
+let bool1: boolean = true;
+let bool2: boolean = false;
+
+// null
+let null1: null = null;
+
+// undefined
+let undefined1: undefined = undefined;
+
+// 리터럴 타입
+// 리터럴 -> 값, 값으로 타입을 정의함
+let numA: 10 = 10;
+// numA = 12;
+
+let strA: "hello" = "hello";
