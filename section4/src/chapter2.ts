dfccc67cca13cdee7427a7b365889a4a7abf4947
서원빈 <1bins@kakao.com>
2025-04-26 19:57:43 +0900

    함수 타입의 호환성

---
+++ section4/src/chapter2.ts
@@ -0,0 +1,62 @@
+/**
+ * 함수 타입 호환성
+ * 특정 함수 타입을 다른 함수 타입으로 취급해도 괜찮은가를 판단함
+ * 1. 반환값의 타입이 호환되는가
+ * 2. 매개변수의 타입이 호환되는가
+ */
+
+// 기준 1. 반환값이 호환되는가
+type A = () => number;
+type B = () => 10;
+
+let a: A = () => 10;
+let b: B = () => 10;
+
+a = b;
+// b = a; ❌ 반환값 기준, 다운 캐스팅
+
+// 기준 2. 매개변수가 호환되는가
+// 2-1. 매개변수의 개수가 같을 때
+type C = (value: number) => void;
+type D = (value: 10) => void;
+
+let c: C = (value) => {};
+let d: D = (value) => {};
+
+// c = d; ❌ 매개변수 기준, 업 캐스팅 안됨
+d = c;
+
+type Animal = {
+  name: string;
+};
+type Dog = {
+  name: string;
+  color: string;
+};
+
+let animalFunc = (animal: Animal) => {
+  console.log(animal.name);
+};
+let dogFunc = (dog: Dog) => {
+  console.log(dog.name);
+  console.log(dog.color);
+};
+
+// Animal 에는 Dog에 있는 color가 없으므로 안됨 아래는 예시
+// animalFunc = dogFunc; ❌
+/* 
+let testFunc = (animal: Animal) => {
+  console.log(animal.name);
+  console.log(animal.color);
+}
+*/
+
+// 2-2. 매개변수의 개수가 다를 때
+type Func1 = (a: number, b: number) => void;
+type Func2 = (a: number) => void;
+
+let func1: Func1 = (a, b) => {};
+let func2: Func2 = (a) => {};
+
+func1 = func2;
+// func2 = func1; ❌ 위에 있는 예제와 같은 느낌, 매개변수가 더 큰 쪽 = 더 작은 쪽 가능
