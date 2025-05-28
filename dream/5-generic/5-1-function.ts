ffda7cddcf5f7e281d251df904e0d6bd38444927
서원빈 <1bins@kakao.com>
2025-05-28 15:27:47 +0900

    제네릭_함수

---
+++ dream/5-generic/5-1-function.ts
@@ -0,0 +1,13 @@
+{
+  function checkNotNull<T>(arg: T | null): T {
+    if (arg == null) {
+      throw new Error('not valid number!');
+    }
+    return arg;
+  }
+
+  const number = checkNotNull(123);
+  const bool = checkNotNull(true);
+
+  console.log(number, bool);
+}
